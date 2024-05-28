afaa7cbbbb1eb76d2b069cfab28218ccc60b2d42
Chenyi Zhao <chenyi.zhao888@gmail.com>
2024-05-27 19:07:33 -0700

    create event page

--- packages/react-front-end/src/App.jsx
+++ packages/react-front-end/src/App.jsx
@@ -14,6 +14,7 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import Home from "./main";
 import Login from "./loginpage";
 import SignUp from "./RegisterUser";
+import AddEvent from "./eventpage";
 import "./App.css";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
 
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/event" element={<AddEvent />} />
 
           {/* If any route mismatches the upper 
           route endpoints then, redirect triggers 
