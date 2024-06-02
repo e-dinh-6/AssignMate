9805c8b02b45ddde7ab03826a09041d4c8252395
Katy Hosokawa <hosokawa@calpoly.edu>
2024-06-01 17:43:03 -0700

    fixed node version

--- jest-unit-testing/services.test.js
+++ jest-unit-testing/services.test.js
@@ -1,6 +1,6 @@
 // services.test.js
 import mongoose from "mongoose";
-import mut from "../packages/backend/services.js";
+import mut from "../packages/backend/services";
 
 // make sure you are running tests while connected to an empty database or tests will fail
 
